653a53d2d2e2cb102c6688b70197d3a8e3f6af3e
Vishnukumar0806 <vishnukumarrajan0806@gmail.com>
2023-09-12 05:57:36 +0530

    updateing

--- src/Comonent/fetchCurrentData.js
+++ src/Comonent/fetchCurrentData.js
@@ -1,43 +1,40 @@
 import React, { useEffect, useState } from "react";
 import './style.css';
-import FetchWithCity from './fetchWithCity.js';
 
 const CurrentWeather = () => {
-    const [weatherData, setWeatherData] = useState([]);
-    const [dailyweatherData, setDailyWeatherData] = useState([]);
+
+    // const [weatherData, setWeatherData] = useState([]);
+    const [dailyweatherData, setDailyWeatherData] = useState('');
     const [longtitude, setlongtitude] = useState('')
     const [latitude, setlatitude] = useState('')
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=0.31&appid=f35723917969d2ffdb6efed8797927b5`;
+
+    // const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=0.31&appid=f35723917969d2ffdb6efed8797927b5`;
     const dailyfeatherForcastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=f35723917969d2ffdb6efed8797927b5`
+
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            // console.log(position.coords.latitude, position.coords.longitude);
-            setlatitude(position.coords.latitude);
-            setlongtitude(position.coords.longitude);
-        });
-        const fetchingData = async (url) => {
+        fetchData(dailyfeatherForcastUrl)
+        console.log(dailyweatherData);
+    }, [latitude, longtitude])
+
+    const fetchData = async (url) => {
+        try {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setlatitude(position.coords.latitude);
+                setlongtitude(position.coords.longitude);
+            });
             const data = await fetch(url);
             const result = await data.json();
-            setWeatherData(result);
-            // console.log(result);
+            setDailyWeatherData(result);
+            console.log(dailyweatherData);
+        } catch (error) {
+            console.log(error);
         }
-        const dailyfeatherForcast = async (url) => {
-            const dailData = await fetch(url);
-            const datilyResult = await dailData.json();
-            // console.log(datilyResult);
-            setDailyWeatherData(datilyResult);
-        }
-        fetchingData(url);
-        dailyfeatherForcast(dailyfeatherForcastUrl);
-        // console.log(typeof (weatherData));
-        console.log(FetchWithCity);
-    }, [latitude, longtitude])
+    }
+
     return (
         <React.Fragment>
             <div className="main-class">
-
                 <p className="heading">Weather Data</p>
-
                 <input type="text"
                     placeholder="latitude..."
                     onChange={(e) => {
@@ -48,33 +45,54 @@ const CurrentWeather = () => {
                     onChange={(e) => {
                         setlongtitude(e.target.value);
                     }} />
-                {
+                    <p>
+                        {
+                            dailyweatherData.timezone
+                        }
+                    </p>
+                  {
                     dailyweatherData.daily &&
-                    dailyweatherData.daily.map(value => {
+                    dailyweatherData.daily.map((value, key) => {
                         return (
-                            <div className="daily-weather-data">
-                                <table style={{ border: '1px solid black' }}>
-                                    <tr>
-                                        <td>
-                                            <label>
-                                                clouds
-                                            </label>
-                                            <p>{value.clouds}</p>
-                                        </td>
-                                        <td>
-                                            <label>humidity</label>
-                                            <p>{value.humidity}</p>
-                                        </td>
-                                        <td>
-                                            <label>sunrise</label>
-                                            <p>{value.sunrise}</p>
-                                        </td>
-                                        <td>
-                                            <label>sunset</label>
-                                            <p>{value.sunset}</p>
-                                        </td>
-                                    </tr>
-                                </table>
+                            <div className="daily-weather-data" key={key}>
+                                <p>
+                                    {value.clouds}
+                                </p>
+                                 {/* <table style={{ border: '1px solid black' }}>
+                                    {
+                                        value.daily((value, key) => {
+                                            return (
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                            <p></p>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <label>
+                                                                clouds
+                                                            </label>
+                                                            <p>{value.clouds}</p>
+                                                        </td>
+                                                        <td>
+                                                            <label>humidity</label>
+                                                            <p>{value.humidity}</p>
+                                                        </td>
+                                                        <td>
+                                                            <label>sunrise</label>
+                                                            <p>{value.sunrise}</p>
+                                                        </td>
+                                                        <td>
+                                                            <label>sunset</label>
+                                                            <p>{value.sunset}</p>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            )
+                                        })
+                                    }
+                                </table>  */}
                                 <div className="otherDetails">
                                     <p>Other details</p>
                                     <p>Day description : {value.weather[0].description}</p>
@@ -83,6 +101,7 @@ const CurrentWeather = () => {
                         )
                     })
                 }
+                
             </div>
 
         </React.Fragment>
